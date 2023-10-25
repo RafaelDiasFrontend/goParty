6a4d78802c6da5ef7ec9d49d03224980bd1623d3
Matheus Dias <matheusdias.front96@gmail.com>
2023-10-25 16:28:08 -0300

    Começo-projeto

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  stats: {
+    warnings: false
+  },
+  mode: 'production',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.[hash].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    }),
+    new CleanWebpackPlugin()
+  ],
+  performance: {
+    hints: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+  }
+};
+
